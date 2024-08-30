378707c3dfe43c541bc7e201adef868fe8d9f06f
Selma <selma.khaje@gmail.com>
2024-08-30 14:39:48 +0330

    add github and url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
 
-function App() {
-  return <div className="App">weather app</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <h1>weather app</h1>
+      <footer>
+        This project was coded by Selma Khajeh and {""}
+        <a href="https://github.com/Selmakhj/weather-react-app" target="_blank">
+          is open-sourced in github
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
